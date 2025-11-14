afad71b1180e7d9d5a15ca2c104ebd241e71eded
Pimentelservicoscuritiba <pimentelservicoscuritiba@gmail.com>
2025-11-14 14:23:08 -0300

    Update robots.js

--- robots.js
+++ robots.js
@@ -1,8 +1,9 @@
-// robots.js – mesmo diretório
+
 export const onRequest = () =>
   new Response(
     `User-agent: *
 Allow: /
 Sitemap: https://pimentelservicos.app.br/sitemap.xml`,
     { headers: { "Content-Type": "text/plain" } }
+
   );
