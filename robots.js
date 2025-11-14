bf401c02979def9b580a1e318c85b11c1a8e717b
Pimentelservicoscuritiba <pimentelservicoscuritiba@gmail.com>
2025-11-14 14:23:29 -0300

    Delete robots.js

--- robots.js
+++
@@ -1,9 +0,0 @@
-
-export const onRequest = () =>
-  new Response(
-    `User-agent: *
-Allow: /
-Sitemap: https://pimentelservicos.app.br/sitemap.xml`,
-    { headers: { "Content-Type": "text/plain" } }
-
-  );
